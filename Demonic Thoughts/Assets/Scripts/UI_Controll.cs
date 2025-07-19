7359775a863a6811d46d0313f4b0e10b9e9b2121
Rosalie Eberhardt aka Rosalie Rousseau <info@RosalieEberhardt.de>
2025-07-19 16:19:38 +0200

    UI Slider music volume

--- Demonic Thoughts/Assets/Scripts/UI_Controll.cs
+++ Demonic Thoughts/Assets/Scripts/UI_Controll.cs
@@ -1,8 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_Controll : MonoBehaviour
 {
+    [SerializeField] GameObject sld_music_Volume;
+    [SerializeField] AudioSource m_AudioSource;
+    private Slider sld_music_Vol_GO;
+    private float sld_music_Vol_Float;
+
+    private void Start()
+    {
+        sld_music_Vol_GO = sld_music_Volume.GetComponent<Slider>();
+    }
+    private void Update()
+    {
+        sld_music_Vol_Float = sld_music_Vol_GO.value;
+    }
 
     public void LoadStartingScene()
     {
@@ -12,5 +26,11 @@ public class UI_Controll : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SetBackgroundVolume()
+    {
+        Debug.Log("Slider Value: " + sld_music_Vol_Float);
+        m_AudioSource.volume = sld_music_Vol_Float;
     }
 }
