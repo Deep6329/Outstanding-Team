b47d6adc8d2a3c96649e7f7b9622af2982fdfe99
Rosalie Eberhardt aka Rosalie Rousseau <info@RosalieEberhardt.de>
2025-07-19 15:47:09 +0200

    Start Button UI implemented

---
+++ Demonic Thoughts/Assets/Scripts/UI_Controll.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Controll : MonoBehaviour
+{
+
+    public void LoadStartingScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
