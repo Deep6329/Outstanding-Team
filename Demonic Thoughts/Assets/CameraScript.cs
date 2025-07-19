46023236fb009ef6cf778134d52b164c5399a51a
Lion <shroompow@gmail.com>
2025-07-19 19:46:13 +0200

    Stuff

--- Demonic Thoughts/Assets/CameraScript.cs
+++ Demonic Thoughts/Assets/CameraScript.cs
@@ -10,5 +10,11 @@ public class CameraScript : MonoBehaviour
     {
         if(trackTarget!=null)
             transform.position = Vector3.Lerp(transform.position, trackTarget.transform.position, 1.0f - Mathf.Pow(1.0f - followSpeed, Time.deltaTime));
+
+        Vector3 eul = transform.eulerAngles;
+
+        eul.y = Mathf.Lerp(eul.y, PlayerBehaviour.Flipped ? 180.0f : 0.0f, 1.0f - Mathf.Pow(1.0f - followSpeed, Time.deltaTime));
+
+        transform.eulerAngles = eul;
     }
 }
